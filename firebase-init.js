358cc2f769aa8fdb7b5c9aa53a56fb67a771b331
dikogp <51299860+dikogp@users.noreply.github.com>
2025-09-28 09:38:13 +0000

    Deploying to gh-pages from @ dikogp/brd-generator@95aebf08e7a56bdd22eda994797cb96827d684a0 🚀

--- firebase-init.js
+++ firebase-init.js
@@ -1,13 +1,12 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
-};
+  apiKey: 'AIzaSyBmGsdOqFAxoeVV2SZ-SU3L37a9tL2qTEc',
+  authDomain: 'brd-generator-app-ce7a5.firebaseapp.com',
+  projectId: 'brd-generator-app-ce7a5',
+  storageBucket: 'brd-generator-app-ce7a5.firebasestorage.app',
+  messagingSenderId: '1083919603735',
+  appId: '1:1083919603735:web:124d11a271434971fa6452',
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 export { db };
